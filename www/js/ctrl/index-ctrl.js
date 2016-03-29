558b82ccedfdacfc6727c151207f9311c118b533
Muhammad Iyas <iyasilias@gmail.com>
2016-03-29 21:33:45 +0800

    Add Cordova Plugin: Touch ID

---
+++ www/js/ctrl/index-ctrl.js
@@ -0,0 +1,5 @@
+app.controller('IndexCtrl', function($scope) {
+ons.ready(function() {
+  // Init code here
+});
+});
---
+++ www/js/ctrl/page-ctrl.js
@@ -0,0 +1,13 @@
+app.controller('PageCtrl', function($scope) {
+ons.ready(function() {
+
+$scope.available = function(){
+	window.plugins.touchid.isAvailable(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
+};
+
+$scope.scan = function(){
+	window.plugins.touchid.verifyFingerprint('Scan your fingerprint please', function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))});
+};
+
+});
+});
