4e8ab8ff80796e292fb466ead8abe4e52856cd36
Muhammad Iyas <iyasilias@gmail.com>
2016-04-01 14:24:42 +0800

    #1.0.3 Try ons-ready

--- js/ctrl/page-ctrl.js
+++ js/ctrl/page-ctrl.js
@@ -1,5 +1,5 @@
 app.controller('PageCtrl', function($scope) {
-//ons.ready(function() {
+ons.ready(function() {
 
 $scope.available = function(){
 	window.plugins.touchid.isAvailable(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
@@ -9,5 +9,5 @@ $scope.scan = function(){
 	window.plugins.touchid.verifyFingerprint('Scan your fingerprint please', function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))});
 };
 
-//});
+});
 });
