ce6747b07cecf9bcc0ba008bd6fb9631ee6202fb
Muhammad Iyas <iyasilias@gmail.com>
2016-04-01 15:03:38 +0800

    Remove parentheses

--- js/ctrl/page-ctrl.js
+++ js/ctrl/page-ctrl.js
@@ -2,7 +2,7 @@ app.controller('PageCtrl', function($scope) {
 //ons.ready(function() {
 
 $scope.available = function(){
-	touchid.checkSupport(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)}));
+	touchid.checkSupport(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
 	//window.plugins.touchid.isAvailable(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
 
 
@@ -10,7 +10,7 @@ $scope.available = function(){
 
 $scope.scan = function(){
 	var text = 'Scan your fingerprint please';
-	
+
 	touchid.authenticate(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))}, text);
 	//window.plugins.touchid.verifyFingerprint('Scan your fingerprint please', function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))});
 };
