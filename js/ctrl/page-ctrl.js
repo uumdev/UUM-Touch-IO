c2501452942b5a172a3ac08c5127eaa3db896832
Muhammad Iyas <iyasilias@gmail.com>
2016-04-01 15:24:11 +0800

    #1.0.5 Use $window

--- js/ctrl/page-ctrl.js
+++ js/ctrl/page-ctrl.js
@@ -1,8 +1,8 @@
-app.controller('PageCtrl', function($scope) {
+app.controller('PageCtrl', function($scope, $window) {
 //ons.ready(function() {
 
 $scope.available = function(){
-	touchid.checkSupport(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
+	$window.touchid.checkSupport(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
 	//window.plugins.touchid.isAvailable(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
 
 
@@ -11,7 +11,7 @@ $scope.available = function(){
 $scope.scan = function(){
 	var text = 'Scan your fingerprint please';
 
-	touchid.authenticate(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))}, text);
+	$window.touchid.authenticate(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))}, text);
 	//window.plugins.touchid.verifyFingerprint('Scan your fingerprint please', function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))});
 };
 
