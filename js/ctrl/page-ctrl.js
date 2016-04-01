b1df7e0d4caf108df5dddebd3fbd16b77fa00b6e
Muhammad Iyas <iyasilias@gmail.com>
2016-04-01 14:10:55 +0800

    #1.0.2 Use real Touch ID function instead of alert()

--- js/ctrl/page-ctrl.js
+++ js/ctrl/page-ctrl.js
@@ -2,11 +2,11 @@ app.controller('PageCtrl', function($scope) {
 //ons.ready(function() {
 
 $scope.available = function(){
-	alert('Available'); //window.plugins.touchid.isAvailable(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
+	window.plugins.touchid.isAvailable(function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + msg)});
 };
 
 $scope.scan = function(){
-	alert('Scan'); //window.plugins.touchid.verifyFingerprint('Scan your fingerprint please', function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))});
+	window.plugins.touchid.verifyFingerprint('Scan your fingerprint please', function(msg) {alert('ok: ' + msg)}, function(msg) {alert('not ok: ' + JSON.stringify(msg))});
 };
 
 //});
